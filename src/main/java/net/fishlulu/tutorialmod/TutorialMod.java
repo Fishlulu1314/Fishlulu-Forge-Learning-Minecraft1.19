81440209c76c65b24831b789dcae9946ea621e9f
Fish_lulu <Fish_lulu1314@fisher.com>
2023-03-18 14:53:29 +0800

    Lesson 1-3 Add custom items,blocks and CreativeModeTab.

--- src/main/java/net/fishlulu/tutorialmod/TutorialMod.java
+++ src/main/java/net/fishlulu/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.fishlulu.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.fishlulu.tutorialmod.block.ModBlocks;
+import net.fishlulu.tutorialmod.item.ModItems;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
@@ -15,11 +17,11 @@ import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TutorialMod.MODID)
+@Mod(TutorialMod.MOD_ID)
 public class TutorialMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "tutorialmod";
+    public static final String MOD_ID = "tutorialmod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -27,6 +29,11 @@ public class TutorialMod
     public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -52,7 +59,7 @@ public class TutorialMod
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientModEvents
     {
         @SubscribeEvent
